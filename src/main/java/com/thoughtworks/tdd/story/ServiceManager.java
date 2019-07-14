672d7ad9e14a97562470224162c58d4c4cd3dd7d
TYG <793557590@qq.com>
2019-07-14 14:02:14 +0800

    refactor: make ServerManager can call park and fetch

--- src/main/java/com/thoughtworks/tdd/story/ServiceManager.java
+++ src/main/java/com/thoughtworks/tdd/story/ServiceManager.java
@@ -3,7 +3,7 @@ package com.thoughtworks.tdd.story;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ServiceManager {
+public class ServiceManager extends ParkingBoy{
     private List<ParkingBoy> parkingBoysList = new ArrayList<>();
     public List<ParkingBoy> getParkingBoysList() {
         return parkingBoysList;
--- src/test/java/com/thoughtworks/tdd/story_test/Story6Test.java
+++ src/test/java/com/thoughtworks/tdd/story_test/Story6Test.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 
 public class Story6Test {
-
+    // AC 1
     @Test
     public void should_return_ticket_when_call_park_given_car(){
         ServiceManager serviceManager = new ServiceManager();
@@ -33,4 +33,22 @@ public class Story6Test {
         Assertions.assertNotEquals(parkingBoysList.size(),0);
     }
 
+    //AC 2
+    @Test
+    public void should_return_ticket_when_call_manager_park_given_car(){
+        ServiceManager serviceManager = new ServiceManager();
+        Car car = new Car();
+        Ticket ticket = serviceManager.park(car);
+
+        Assertions.assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_return_car_when_call_manager_fetch_given_ticket(){
+        ServiceManager serviceManager = new ServiceManager();
+        Ticket ticket = serviceManager.park(new Car());
+
+        Car car = serviceManager.fetch(ticket);
+        Assertions.assertNotNull(car);
+    }
 }
